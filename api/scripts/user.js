d544c92408135d3d837f7b27e694e505dd252991
joppimenta <joaopedropimenta@alu.ufc.br>
2024-09-10 18:14:46 -0300

    get users add

--- api/scripts/user.js
+++ api/scripts/user.js
@@ -1,9 +1,21 @@
-function getDados(){
-    fetch('../data/users.json')
+function loadDocJSON(url, id) {
+  fetch(url)
     .then(response => {
-        if (!response.ok) {
-          throw new Error('Erro na requisição: ' + response.status);
-        }
-        return response.json();
-      })
+      response.json().then((data) => {
+        document.getElementById(id).innerHTML = arrToUl(data);
+      });
+    }).catch(err => {
+      console.error('Failed retrieving information', err);
+    });
 }
+
+function arrToUl(response) {
+  lista = response.lista;
+  str = "<h3>" + response.nome + "</h3> <ul>";
+  for (i = 0; i < lista.length; i++)
+    str += "<li>" + lista[i] + "</li>";
+  str += "</ul>";
+  return str;
+}
+
+loadDocJSON("../data/users.json", "demo2");
