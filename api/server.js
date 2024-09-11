09f38ee7f622b1f1765ddeb67226225cc0f92186
fabio-gabriel <fabiogabrieleig@gmail.com>
2024-09-10 22:23:08 -0300

    refactor all views to ejs

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8085
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+// what
+// const defaultGET = require('./src/defaultGET')
+// const defaultPOST = require('./src/defaultPOST')
+
+app.listen(port, function () {
+	console.log('Server listening on port ' + port)
+})
