24288a2f4295c678b17104d69ffbd186c652b942
fabio-gabriel <fabiogabrieleig@gmail.com>
2024-09-11 15:20:29 -0300

    adds fetch without axios

--- ui/controllers/homeController.js
+++ ui/controllers/homeController.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default
+const http = require('http');
 const user1 = { id: 1, username: 'pimenta', role: 'admin' } 
 const user2 = { id: 2, username: 'fabio', role: 'user' }
 const userV = undefined
@@ -21,52 +22,43 @@ class HomeController{
   }
 
   /*
-  async login(req, res){
-    res.render('pages/login', {
-      title: "Login"
-    })
-  }
-
-  async authenticate(req, res){
-    const email = req.body["email"]
-    const password = req.body["password"]
-
-    const response = await axios.post('http://localhost:5000/authenticate', {
-      email: email,
-      password: password
-    }).catch((error) => {
-      res.redirect('/login')
-    })  
-
-    if(response.status == 200){
-      res.cookie("session_token", response.data.session_token)
-      return res.redirect('/administrative');
+  async function index(req, res) {
+    try {
+      const url = 'http://localhost:8084/';
+      
+      const fetchData = () => {
+        return new Promise((resolve, reject) => {
+          http.get(url, (response) => {
+            let data = '';
+  
+            response.on('data', (chunk) => {
+              data += chunk;
+            });
+  
+            response.on('end', () => {
+              try {
+                const jsonRes = JSON.parse(data);
+                resolve(jsonRes);
+              } catch (error) {
+                reject(error);
+              }
+            });
+          }).on('error', (error) => {
+            reject(error);
+          });
+        });
+      };
+  
+      const jsonRes = await fetchData();
+      res.render('index', {
+        title: "Home",
+        auctions: jsonRes.auctions,
+        user: userV
+      });
+    } catch (error) {
+      console.log(error.message);
     }
-    return res.redirect('/login')
-  }
-
-  async logout(req, res){
-    const session_token = req.cookies["session_token"]
-
-    const response = await axios.post('http://localhost:5000/logout',{},{
-      headers:{
-        'Cookie': `session_token=${session_token}`
-      }
-    }).catch((error) => {
-      console.log(error.message)
-    }) 
-
-    res.redirect('/login')
-    return res.end() 
-  }
-
-  async forgotPassword(req, res){
-    res.render('pages/forgotPassword', {
-      title: "Recuperar senha",
-      baseUrl: req.baseUrl
-    })
-  }
-    */
+  } */
 }
 
 module.exports = new HomeController
